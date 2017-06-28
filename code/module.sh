ab4475a6674f3d2bbe47fb042d1f47d9e4ff6eb7
Marc Sze <marcasze@gmail.com>
2017-06-28 18:42:29 -0400

    add bedtools to modules to load

--- code/module.sh
+++ code/module.sh
@@ -8,6 +8,7 @@ module load sratoolkit/2.8.2-1
 module load samtools/1.3.1
 module load bowtie2/2.1.0
 module load sickle/1.33.6
+module load bedtools2/2.20.1
 
 # Control pathing
 export BOWTIE2_INDEXES=/nfs/turbo/schloss-lab/msze/active_projects/metagenome_practice/data/references
